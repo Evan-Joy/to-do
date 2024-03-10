fa3327e0f39d260f18daeb71d3f7a2ec629bc8e6
Evan Joy <phucsinnet113@gmail.com>
2024-03-10 19:25:03 +0700

    Configuration BE

--- be/run-db.sh
+++ be/run-db.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cp_dev="MY_docker-compose.dev.yml"
-cp_prod="MY_docker-compose.prod.yml"
+cp_dev="PG_docker-compose.dev.yml"
+cp_prod="PG_docker-compose.prod.yml"
 
 read -p "Enter to select mode [dev/prod]: " MODE
 
 if [[ "$MODE" == "prod" ]]; then
-  sudo docker-compose --project-name app-nestjs-base  --env-file .env -f db-compose/$cp_prod up -d --build
+  sudo docker-compose --project-name to-do-base  --env-file .env -f db-compose/$cp_prod up -d --build
 else
-  sudo docker-compose --project-name app-nestjs-base  --env-file .env -f db-compose/$cp_dev up -d --build
+  sudo docker-compose --project-name to-do-base  --env-file .env -f db-compose/$cp_dev up -d --build
 fi
